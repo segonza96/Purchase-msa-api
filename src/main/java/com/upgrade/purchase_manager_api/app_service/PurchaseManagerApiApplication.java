0fdfb16bae758a5b34368f6850973c3dd03af561
segonzaG <exsgonzale@consubanco.com>
2024-09-29 21:35:58 -0500

    initial commit

---
+++ src/main/java/com/upgrade/purchase_manager_api/app_service/PurchaseManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.purchase_manager_api.app_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/upgrade/purchase_manager_api/infraestructure/entry_points/PurchaseOrderLoaderController.java
@@ -0,0 +1,20 @@
+package com.upgrade.purchase_manager_api.infraestructure.entry_points;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value = "/purchase", produces = MediaType.APPLICATION_JSON_VALUE)
+
+public class PurchaseOrderLoaderController {
+
+    @PostMapping(value = "/load")
+    public ResponseEntity<Object> load(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().build();
+    }
+}
